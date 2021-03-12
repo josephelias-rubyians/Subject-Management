cc532e9c4aa0542c47bd1ea9ad235e1e07a4b3ec
Joseph Elias <joseph.elias@rubyians.com>
2021-03-12 11:24:13 +0530

    Validations added

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
 class Subject < ApplicationRecord
-	has_many :sub_and_classes
+	has_many :sub_and_classes, dependent: :destroy
 	has_many :teaching_classes, through: :sub_and_classes
+
+	validates :name, presence: true
 
 end
