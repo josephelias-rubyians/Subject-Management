22106f3671b217bac3b1eba29f931838dd65f5bb
Joseph Elias <joseph.elias@rubyians.com>
2021-03-12 17:45:10 +0530

    Rspec for sign up and sign in

---
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe Users::RegistrationsController, type: :request do
+
+  let (:user) { build_user }
+  let (:existing_user) { create_user }
+  let (:signup_url) { '/signup' }
+  let(:params) do
+    {
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    }
+  end
+
+  context 'When creating a new user' do
+    before do
+      post signup_url, params: params
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns a token' do
+      expect(response.headers['Authorization']).to be_present
+    end
+
+    it 'returns the user email' do
+      expect(json["email"]).to eq(user.email)
+    end
+  end
+
+  context 'When an email already exists' do
+    before do
+      post signup_url, params: {
+        user: {
+          email: existing_user.email,
+          password: existing_user.password
+        }
+      }
+    end
+
+    it 'returns 422' do
+      expect(response.status).to eq(422)
+    end
+  end
+
+end
