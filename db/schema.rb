fc498898ffcc4028a69c1febd4781af6d3985001
Joseph Elias <joseph.elias@rubyians.com>
2021-03-26 14:21:59 +0530

    Removed default of email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_064332) do
+ActiveRecord::Schema.define(version: 2021_03_26_084434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_064332) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "password"
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
